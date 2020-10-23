4154d56addbff7764ccb9ac947883dd7e95222f6
uday94043 <uday94043@gmail.com>
2020-10-23 20:43:02 +0530

    check generation strategy for id

--- src/main/java/com/paysafe/roiim/resources/UserEntity.java
+++ src/main/java/com/paysafe/roiim/resources/UserEntity.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 @Entity
 public class UserEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String email;
     private String customerId;
